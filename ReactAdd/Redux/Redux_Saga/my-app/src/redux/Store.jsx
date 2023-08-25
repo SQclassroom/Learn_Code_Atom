c663093a9cd8f8c77ea4818309b933125931e6c4
SQclassroom <sq.vivek@gmail.com>
2023-08-25 13:53:39 +0530

    25-08-2023

---
+++ ReactAdd/Redux/Redux_Saga/my-app/src/redux/Store.jsx
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+
+const dummyReducer = () =>{
+     return 100
+}
+
+// const store = createStore(() => {
+
+// })
+
+const store = createStore (dummyReducer)
+
+export default store
